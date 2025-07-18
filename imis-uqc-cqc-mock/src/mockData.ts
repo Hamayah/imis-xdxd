a0706fb4d72152d2d9ad794812d0a3ea90dcdb46
Nicholas Wong <e0727219@u.nus.edu>
2025-07-18 17:57:30 +0800

    Move video to public folder and update mockData to use public path for Netlify deployment

--- imis-uqc-cqc-mock/src/mockData.ts
+++ imis-uqc-cqc-mock/src/mockData.ts
@@ -25,8 +25,8 @@ export interface CaseData {
 export const MOCK_CASE:CaseData = {
   room_id: '7525690569877507333',
   extra_info: {"avg_cu":"","best_point_time":"1752280668","create_time":"1752280987","data_type":"2","diamond_count":"99.0","diamond_time":"1752280668.0","duration":"13.016","end_time":"1752280678","game_time":"","generate_total_ms":"36982","input_msg":"","is_talent":"","level":"","moment_type":"","output_result":"","prompt_version":"v0","region":"BR","result_key":"PreviewHighlight_1752280988389_2e19f03b-5eb9-11f0-a8f1-3436ac12008d","reviewer":"nicholaswong","start_time":"1752280665","surge_count":"","surge_rage":"","surge_time":"","surge_time_pv":"","total_like_pv":"","url":"https://v16m-ttgl.tiktokcdn.com/7ddee2434906ab49d6314168c7d4b93a/68993ccf/video/tos/maliva/tos-maliva-v-48829e-us/ogvqEfvph0sTBA4x6PAKEU5roAi7gaCGHLBkAi/?a=1233&bti=MzY5QDQyM2A%3D&ch=0&cr=0&dr=0&cd=0%7C0%7C0%7C0&br=1996&bt=998&ds=4&ft=Gc-1XInz7ThhwfmKXq8Zmo&mime_type=video_mp4&qs=13&rc=anQ3eG45cmZ0NDQzN2Q6M0BpanQ3eG45cmZ0NDQzN2Q6M0BkazRoMmRrc2xhLS1kYjZzYSNkazRoMmRrc2xhLS1kYjZzcw%3D%3D&vvpl=1&l=02175227966589500000000000000000000ffff0a79581286926f&btag=e00070000&download=true&filename=1752281026.mp4"},
-  highlightUrl: 'src/assets/1752281026.mp4',
+  highlightUrl: '/1752281026.mp4',
   highlightDuration: 13.016,
-  fullUrl: 'src/assets/1752281026.mp4',
+  fullUrl: '/1752281026.mp4',
   fullDuration: 13.016
 }; 
